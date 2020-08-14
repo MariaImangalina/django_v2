a2013c9b7f8c13fdf5de7e8e2b42fa28845528a7
geo_varenik <varenik_geo@mail.ru>
2020-08-14 14:36:26 +0300

    add allowed host

--- learn_auth/settings.py
+++ learn_auth/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '*#2l(vfuzce2br72tsh$5n2lq)ey3r@=!firn6u%@fl!y&sa!y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mariaimangalina.pythonanywhere.com']
 
 
 # Application definition
